d8f694bd542223cf16d985614bff5693485b346d
xingsheng.gao <xingsheng.gao@kaiqigu.com>
2021-02-07 15:46:35 +0800

    添加et

---
+++ Unity/Assets/EGamePlay/EGamePlayInit.cs
@@ -0,0 +1,29 @@
+﻿using EGamePlay;
+using ET;
+using UnityEngine;
+
+
+public class EGamePlayInit : MonoBehaviour
+{
+    private void Awake()
+    {
+        EntityFactory.Global = new GlobalEntity();
+        EntityFactory.CreateWithParent<TimerComponent>(EntityFactory.Global);
+    }
+
+    private void Start()
+    {
+//        DontDestroyOnLoad(this);
+    }
+
+    private void Update()
+    {
+        EntityFactory.Global.Update();
+        TimerComponent.Instance.Update();
+    }
+
+    private void OnApplicationQuit()
+    {
+        Entity.Destroy(EntityFactory.Global);
+    }
+}
